5d1566b0d128d572dadccc3cb283c94b96ef0194
MrSakamoto-san <me@felis.cat>
2017-06-28 13:47:46 -0400

    rush b

---
+++ Constants.js
@@ -0,0 +1,13 @@
+module.exports = {
+    BASE_URL: 'http://api.wolframalpha.com',
+    POD: class Pod {
+       constructor({}) {
+          return new Error('to be done later luke needed this fast');
+       }
+    },
+    RESPONSE: class Response {
+       constructor({}) {
+          return new Error('to be done later luke needed this fast');
+       }
+    }
+};
---
+++ wolfram.js
@@ -0,0 +1,31 @@
+const snekfetch = require('snekfetch');
+const { BASE_URL, RESPONSE } = require('./Constants');
+
+class Wolfram {
+    constructor(appId) {
+        if (!appId) throw new ReferenceError('appId is not defined');
+        this.appId = appId;
+    }
+
+    query(query, options = {}) {
+        return new snekfetch('GET', `${BASE_URL}/v2/query`)
+	.query({
+appid: this.key,
+input: encodeURIComponent(query)
+primary: true,
+output: 'json'
+})
+        .then(res => res.body && res.body.queryresult.success ? res.body : Promise.reject(TypeError('no results')));
+    }
+    
+    get key() {
+        return this.appId() || this.appId[0] || this.appId;
+    }
+    
+    get version() {
+        return require('./package.json').version;
+    }
+
+}
+
+module.exports = Wolfram;
